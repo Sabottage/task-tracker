c49ffd8a0306c62a27ab3815aa3cc2fb2e0f3d9f
Ion Macovei <macoveii300@gmail.com>
2025-01-13 18:59:53 +0200

    Added MongoDB connection code

--- src/server.js
+++ src/server.js
@@ -22,3 +22,13 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('Error: ' + err));
